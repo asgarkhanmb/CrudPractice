6170c012f9f9f0a2982cf800a53f4873042e7f23
unknown <asgarkhanmb@code.edu.az>
2024-05-29 23:50:04 +0400

    Practice

---
+++ Fiorello-PB101/Fiorello-PB101/ViewModels/Products/ProductImageVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Fiorello_PB101.ViewModels.Products
+{
+    public class ProductImageVM
+    {
+  
+        public string Image { get; set; }
+        public bool IsMain { get; set; }
+    }
+}
